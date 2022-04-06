95a9d273c77098c035165edf7c3162098658df4e
admin <godsonezekiel31@gmail.com>
2022-04-06 21:47:52 +0300

    some changes

--- working-1.py
+++ working-1.py
@@ -31,21 +31,21 @@ with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0
         image=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         result=holistic.process(image)
         image=cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-        mp_drawing_utils.draw_landmarks(
-        image,
-        result.face_landmarks,
-        mp_holistic.FACEMESH_TESSELATION,
-        landmark_drawing_spec=None,
-        connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
-        mp_drawing_utils.draw_landmarks(image,result.pose_landmarks,mp_holistic.POSE_CONNECTIONS)
-        mp_drawing_utils.draw_landmarks(image,result.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
-        mp_drawing_utils.draw_landmarks(image,result.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
+        #mp_drawing_utils.draw_landmarks(
+        #image,
+        #result.face_landmarks,
+        #mp_holistic.FACEMESH_TESSELATION,
+        #landmark_drawing_spec=None,
+        #connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
+        #mp_drawing_utils.draw_landmarks(image,result.pose_landmarks,mp_holistic.POSE_CONNECTIONS)
+        #mp_drawing_utils.draw_landmarks(image,result.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
+        #mp_drawing_utils.draw_landmarks(image,result.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
         #image = cv2.resize(image, (1000,7000))
         #cv2.imshow('raw',image)
         try:
             coord1=tuple(np.multiply(np.array((result.face_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_EAR].x,result.face_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_EAR].y)),[640,480]).astype('int'))
             print(coord1)
-            coord1=np.array(coord1)+np.array((70,-100))
+            coord1=np.array(coord1)+np.array((80,-110))
             coord2=tuple(np.multiply(np.array((result.face_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x,result.face_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y)),[640,480]).astype('int'))
             
             coord2=np.array(coord2)+np.array((-40,100))
